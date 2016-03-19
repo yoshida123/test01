5a783558c6d35534504788f4b6eb4080a2d9addd
yoshida123 <cs.fact.yoshida@gmail.com>
2016-03-19 11:46:29 +0900

    a

---
+++ Java_yoshida/Quest01Imafuku.java
@@ -0,0 +1,15 @@
+package com.cs_fact.educ.ysd.java1.etc.ques;
+
+public class Quest01Imafuku {
+
+	
+	public static void main(String[] args) {
+		SetterGetter1 c = new SetterGetter1();
+		c.setNamae("今福");
+		c.setNenrei(92);
+		c.setHeyabangou(13);
+		
+		c.print();
+	}
+
+}
---
+++ Java_yoshida/Quest01Yoshsida.java
@@ -0,0 +1,15 @@
+package com.cs_fact.educ.ysd.java1.etc.ques;
+
+public class Quest01Yoshsida {
+
+	
+	public static void main(String[] args) {
+		SetterGetter1 b = new SetterGetter1();
+		b.setNamae("吉田");
+		b.setNenrei(8);
+		b.setHeyabangou(4);
+		
+		b.print();
+	}
+
+}
